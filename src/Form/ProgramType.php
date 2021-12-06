42843c84cbc1f418e962b27443cfaad33d857153
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-12-06 14:27:25 +0100

    add actors

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,7 +22,13 @@ class ProgramType extends AbstractType
             ->add('country', TextType::class)
             ->add('year', NumberType::class)
             ->add('category', null, ['choice_label' => 'name'])
-        ;
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
